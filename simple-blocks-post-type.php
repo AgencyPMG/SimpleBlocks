e96724dbc3f5550a776286528c186e85878c49c9
Emily Fox <hello@emilyfox.co.nz>
2014-09-30 10:56:36 -0500

    A WP plugin that creates a custom post type with a shortcode
    A WordPress plugin that creates a non-public  post type called 'Simple
    Blocks'. The information entered into this custom post type can
    then be pulled out into the content via a shortcode [simpleblock].

---
+++ simple-blocks-post-type.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: Simple Blocks Post Type
+ * Plugin URI: http://pmg.co/
+ * Description: Creates a custom post type that you can pull through via a shortcode
+ * Version: 1.0
+ * Text Domain: marklogic
+ * Author: Emily Fox
+ * Author URI: http://pmg.co/
+ * License: GPL-2.0+
+ *
+ * Copyright 2014 Performance Media Group <http://pmg.co>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+!defined('ABSPATH') && exit;
+
+define('SIMPLE_BLOCKS_URL', dirname(__FILE__));
+
+require_once SIMPLE_BLOCKS_URL . '/inc/custom-post-type.php';
+add_action('init', 'createSimpleBlocksPostType'); 
+ 
+?>
