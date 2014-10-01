649e871fe575ba2f9d31def47c47f52c9a177c64
Emily Fox <hello@emilyfox.co.nz>
2014-10-01 16:51:51 -0500

    Cleaning file formatting accross all files #10
    Cleaning up file formatting to ensure the structure of the php
    code is correct, with the correct line breaks.

--- inc/Shortcode.php
+++ inc/Shortcode.php
@@ -12,6 +12,7 @@
  * @copyright   2014 PMG <http://pmg.co>
  * @license     http://opensource.org/licenses/Apache-2.0 Apache-2.0
  */
+
 namespace PMG\SimpleBlocks;
 
 !defined('ABSPATH') && exit;
--- simple-blocks-post-type.php
+++ simple-blocks-post-type.php
@@ -24,6 +24,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
 !defined('ABSPATH') && exit;
 
 require_once __DIR__.'/inc/Setup.php';
