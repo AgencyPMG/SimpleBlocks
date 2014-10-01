9483bdb317270371b13df9080f3677be2debfa4a
Emily Fox <hello@emilyfox.co.nz>
2014-10-01 16:50:40 -0500

    Added docblock to the Setup.php file #8
    Including a docblock section at the top of inc/Setup.php

--- inc/Setup.php
+++ inc/Setup.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * This file is part of Simple Blocks Plugin, it is called by other
+ * classes to setup the plugin.
+ *
+ * Copyright (c) 2014 PMG <http://pmg.co>
+ *
+ * For full copyright and license information please see the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @category    WordPress
+ * @copyright   2014 PMG <http://pmg.co>
+ * @license     http://opensource.org/licenses/Apache-2.0 Apache-2.0
+ */
+
 namespace PMG\SimpleBlocks;
 
 !defined('ABSPATH') && exit;
